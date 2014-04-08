bee61e1dc3855cc0ed6bc3f39bcbcfe70b3ca90a
ShinDarth <borzifrancesco@gmail.com>
2014-04-08 14:21:32 +0200

    title as link in index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                 </ul>
               </li>
             </ul>
-		<h3 class="text-muted">SQL-Injection Demo</h3>
+		<h3 class="text-muted"><a href="index.php">SQL-Injection Demo</a></h3>
       </div>
 		
 	<div class="jumbotron">
